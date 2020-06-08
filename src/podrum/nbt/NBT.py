cda560b4bea2c1b85911da918436466d2e5c4a6d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-08 11:32:39 +0300

    Update NBT.py

--- src/podrum/nbt/NBT.py
+++ src/podrum/nbt/NBT.py
@@ -11,4 +11,25 @@
 * (at your option) any later version.
 """
 
-from src.Podrum.utils 
+from src.Podrum.utils import Binary
+
+class NBT:
+    LITTLE_ENDIAN = 0
+    BIG_ENDIAN = 1
+    TAG_End = 0
+    TAG_Byte = 1
+    TAG_Short = 2
+    TAG_Int = 3
+    TAG_Long = 4
+    TAG_Float = 5
+    TAG_Double = 6
+    TAG_ByteArray = 7
+    TAG_String = 8
+    TAG_List = 9
+    TAG_Compound = 10
+    TAG_IntArray = 11
+    
+    buffer = None
+    offset = None
+    endianness = None
+    data = None
